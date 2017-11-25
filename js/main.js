b920e8e5da6101f360c4b5ae3005c7298f8e767f
Alokhin Anton <alokhin.anton@gmail.com>
2017-11-25 12:11:39 +0200

    some more

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-document.getElementsByClassName('up')[0]
+var r;
+document.getElementsByClassName('up')[0].addEventListener('dblclick',up);
+document.getElementsByClassName('down')[0].addEventListener('dblclick',down);
+document.getElementsByClassName('moveToPoint')[0].addEventListener('dblclick',function (event) {
+    console.log(event);
+    r = event.target;
+    moveToPoint(1,1);
+});
+document.getElementsByClassName('moveToVector')[0].addEventListener('dblclick',function (event) {
+    console.log(event.target);
+    moveToVector(1, 1);
+});
