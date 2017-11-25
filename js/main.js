34b7d3797e8210f9bf271d11a73534415a023901
Alokhin Anton <alokhin.anton@gmail.com>
2017-11-25 12:37:14 +0200

    add jquery

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
 var r;
-document.getElementsByClassName('up')[0].addEventListener('dblclick',up);
-document.getElementsByClassName('down')[0].addEventListener('dblclick',down);
-document.getElementsByClassName('moveToPoint')[0].addEventListener('dblclick',function (event) {
-    console.log(event);
-    r = event.target;
-    moveToPoint(1,1);
+function getCoords(el){
+    var x,y;
+    console.log(el);
+    if (el && el.children) {
+        x = event.target.children[0].value;
+        y = event.target.children[1].value;
+    }
+    return {x:x,y:y}
+
+}
+$('.up').on('dblclick', up);
+$('.down').on('dblclick', down);
+$('.moveToPoint').on('dblclick', function(e) {
+    if (e.target !== this)
+        return;
+    var coords = getCoords(event.target),
+        x = +coords.x,
+        y = +coords.y;
+    console.log(x,y);
+    if(x && y)
+        moveToPoint(x, y);
 });
-document.getElementsByClassName('moveToVector')[0].addEventListener('dblclick',function (event) {
-    console.log(event.target);
-    moveToVector(1, 1);
+$('.moveToVector').on('dblclick', function(e) {
+    if (e.target !== this)
+        return;
+    var coords = getCoords(event.target),
+        x = +coords.x,
+        y = +coords.y;
+    console.log(x,y);
+    if(x && y)
+        moveToVector(x, y);
 });
