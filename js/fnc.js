47e0c57d774ba3bb72ddd34a5de2ac35ee7c5f0d
Alokhin Anton <alokhin.anton@gmail.com>
2017-11-25 12:56:17 +0200

    fixed, added pen

--- js/fnc.js
+++ js/fnc.js
@@ -1,7 +1,12 @@
 var board = JXG.JSXGraph.initBoard('jxgbox', {keepaspectratio: true, boundingbox: [-10, 10, 10, -10], axis:true});
-
-var pen = {x:0,y:0, active: up};
 var pointParams = {size: 1,style:{color:'black'}};
+
+var pen = {x:0,y:0, active: up,
+    view: board.create('point',[0,0],Object.assign({},pointParams,{color: "black"}))
+    };
+function updatePen(){
+    pen.view.moveTo([pen.x,pen.y]);
+}
 function drawPoint(x,y){
     return  board.create('point',[x,y],pointParams);
 }
@@ -14,10 +19,12 @@ function up(){
     pen.active = false;
 }
 function drawLineTo(x,y){
-    var line,p1,p2;
+    var line,
+        p1,
+        // p2;
     p1 = drawPoint(pen.x,pen.y);
-    p2 = drawPoint(x,y);
-    line = board.create('line',[p1,p2], {straightFirst:false, straightLast:false, strokeWidth:2});
+    // p2 = drawPoint(x,y);
+    line = board.create('line',[p1,[x,y]], {straightFirst:false, straightLast:false, strokeWidth:2});
 //            moveTo(x,y);
     return line;
 }
@@ -27,6 +34,7 @@ function moveToPoint(x,y){
         drawLineTo(x,y);
     pen.x = x;
     pen.y = y;
+    updatePen();
 }
 function moveToVector(x,y){
     console.log("move to vector",x,y);
@@ -34,4 +42,5 @@ function moveToVector(x,y){
         drawLineTo(pen.x+x,pen.y+y);
     pen.x += x;
     pen.y += y;
+    updatePen();
 }
