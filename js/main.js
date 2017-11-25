0977c02cbe1a8ac351ce7e2002f8b6167d1c7a0b
Alokhin Anton <alokhin.anton@gmail.com>
2017-11-25 16:05:02 +0200

    norm

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var r;
+var cmdHistory = [];
 function getCoords(el){
     var x,y;
     console.log(el);
@@ -9,41 +9,75 @@ function getCoords(el){
     return {x:x,y:y}
 
 }
-$('.up').on('dblclick', up);
-$('.down').on('dblclick', down);
-$('.moveToPoint').on('dblclick', function(e) {
-    if (e.target !== this)
-        return;
-    var coords = getCoords(event.target),
-        x = coords.x,
-        y = coords.y;
-    console.log(x,y);
-    if(x && y)
-        moveToPoint(x, y);
-});
-$('.moveToVector').on('dblclick', function(e) {
-    if (e.target !== this)
-        return;
-    var coords = getCoords(event.target),
-        x = coords.x,
-        y = coords.y;
-    console.log(x,y);
-    if(x && y)
-        moveToVector(x, y);
-});
+var cmd = function(e) {
+    executeCmd(parseCmd(e.target));
+};
+$('.up').on('dblclick', cmd);
+$('.down').on('dblclick', cmd);
+$('.moveToPoint').on('dblclick', cmd);
+$('.moveToVector').on('dblclick', cmd);
 
-
+function parseCmd(el) {
+    var cmd = {command: el.getAttribute('data-action')};
+    if (cmd.command === 'moveToPoint' || cmd.command ==='moveToVector'){
+        cmd.coords = getCoords(el);
+    }
+    return cmd;
+}
 function parseCmdList() {
     var list = [];
     $('.commandList').children('.ElementList').each(function (_,el) {
-        var cmd = {command: el.getAttribute('data-action')};
-        if (cmd.command === 'moveToPoint' || cmd.command ==='moveToVector'){
-            cmd.coords = getCoords(el);
-            }
-        list.push(cmd);
+        // var cmd = {command: el.getAttribute('data-action')};
+        // if (cmd.command === 'moveToPoint' || cmd.command ==='moveToVector'){
+        //     cmd.coords = getCoords(el);
+        //     }
+
+        list.push(parseCmd(el));
     });
     console.log(list);
     return list;
+}
+
+function validateCmdList(list) {
+    for(var cmd in list){
+        if(!validateCmd(cmd))
+            return false;
+    }
+    return true;
+}
+function validateCmd(cmd) {
+    console.log(cmd);
+    if(availableCommands.indexOf(cmd.command)===-1)
+        return false;
+    if(cmd.command === 'moveToPoint' || cmd.command ==='moveToVector')
+        if (isNaN(cmd.coords.x) ||
+            isNaN(cmd.coords.y))
+            return false
+    return true;
+}
+function executeCmd(cmd){
+    if (validateCmd(cmd)){
+        switch (cmd.command){
+            case 'up': up(); break;
+            case 'down': down(); break;
+            case 'moveToPoint': moveToPoint(cmd.coords.x,cmd.coords.y); break;
+            case 'moveToVector':  moveToVector(cmd.coords.x,cmd.coords.y); break;
+        }
+        cmdHistory.push(cmd);
+    }
+}
+function executeCmdList(list) {
+    // if (!validateCmdList()) {
+    //     console.log("not valid");
+    //     return false;
+    // }
+    var funcList = [],f;
+    for(var cmd of list){
+        console.log(cmd);
+        executeCmd(cmd);
+    }
+
+    return true;
 }
 
 $('.execute').on('click', function () {
