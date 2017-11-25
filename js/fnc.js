dd9157e387cf7edef5968532652c4a2241f8bd70
Alokhin Anton <alokhin.anton@gmail.com>
2017-11-25 15:00:17 +0200

    some more

--- js/fnc.js
+++ js/fnc.js
@@ -1,6 +1,8 @@
-var board = JXG.JSXGraph.initBoard('jxgbox', {keepaspectratio: true, boundingbox: [-10, 10, 10, -10], axis:true});
-var pointParams = {size: 1,style:{color:'black'}};
-
+var board = JXG.JSXGraph.initBoard('jxgbox',
+    {keepaspectratio: true, boundingbox: [-5, 5, 5, -5], axis:true}
+    );
+var pointParams = {size: 1,style:{color:'black',fixed:true}};
+var availableCommands = ['up','down','moveToPoint','moveToVector'];
 var pen = {x:0,y:0, active: up,
     view: board.create('point',[0,0],Object.assign({},pointParams,{color: "black"}))
     };
@@ -32,6 +34,8 @@ function moveToPoint(x,y){
     console.log("move to point",x,y);
     if (pen.active)
         drawLineTo(x,y);
+    else
+        drawPoint(pen.x,pen.y);
     pen.x = x;
     pen.y = y;
     updatePen();
@@ -40,7 +44,50 @@ function moveToVector(x,y){
     console.log("move to vector",x,y);
     if (pen.active)
         drawLineTo(pen.x+x,pen.y+y);
+    else
+        drawPoint(pen.x,pen.y);
     pen.x += x;
     pen.y += y;
     updatePen();
+}
+function validateCmdList(list) {
+    for(var cmd in list){
+        if(!availableCommands.indexOf(cmd.command)!==-1)
+            return false;
+        if(cmd.command === 'moveToPoint' || cmd.command ==='moveToVector')
+            if (!(typeof cmd.coords.x  === 'number' &&
+                typeof cmd.coords.y   === 'number' ))
+                return false
+    }
+    return true;
+}
+function executeCmdList(list) {
+    if (!validateCmdList()) {
+        console.log("not valid");
+        return false;
+    }
+    var funcList = [],f;
+    for(var cmd of list){
+        console.log(cmd);
+        switch (cmd.command){
+            case 'up': f=up; break;
+            case 'down': f=down; break;
+            case 'moveToPoint': f=moveToPoint.bind(null,cmd.coords.x,cmd.coords.y); break;
+            case 'moveToVector':  f=moveToVector.bind(null,cmd.coords.x,cmd.coords.y); break;
+        }
+        funcList.push(f);
+    }
+    console.log(funcList);
+    function i(f){
+        console.log(f);
+        window.setTimeout(function(){
+            f();
+            f2 = funcList.pop();
+            if(f2)
+                i(f2);
+        }, 1 * 1000);
+
+    }
+    i(funcList.pop());
+    return true;
 }
--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ function getCoords(el){
     var x,y;
     console.log(el);
     if (el && el.children) {
-        x = event.target.children[0].value;
-        y = event.target.children[1].value;
+        x = +el.children[0].value;
+        y = +el.children[1].value;
     }
     return {x:x,y:y}
 
@@ -15,8 +15,8 @@ $('.moveToPoint').on('dblclick', function(e) {
     if (e.target !== this)
         return;
     var coords = getCoords(event.target),
-        x = +coords.x,
-        y = +coords.y;
+        x = coords.x,
+        y = coords.y;
     console.log(x,y);
     if(x && y)
         moveToPoint(x, y);
@@ -25,9 +25,27 @@ $('.moveToVector').on('dblclick', function(e) {
     if (e.target !== this)
         return;
     var coords = getCoords(event.target),
-        x = +coords.x,
-        y = +coords.y;
+        x = coords.x,
+        y = coords.y;
     console.log(x,y);
     if(x && y)
         moveToVector(x, y);
+});
+
+
+function parseCmdList() {
+    var list = [];
+    $('.commandList').children('.ElementList').each(function (_,el) {
+        var cmd = {command: el.getAttribute('data-action')};
+        if (cmd.command === 'moveToPoint' || cmd.command ==='moveToVector'){
+            cmd.coords = getCoords(el);
+            }
+        list.push(cmd);
+    });
+    console.log(list);
+    return list;
+}
+
+$('.execute').on('click', function () {
+    executeCmdList(parseCmdList());
 });
